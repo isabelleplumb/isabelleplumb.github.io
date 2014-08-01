7bdedfc8f0a8d40dd3a5f79e2747a67ea95cf555
isabelleplumb <izzykat13@gmail.com>
2014-07-31 20:35:41 -0700

    10. Small Details

---
+++ head.php
@@ -0,0 +1,7 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>OurWorks</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
